c16c580519fbc860b3883db6a228fa3b865ba774
usho1024 <usho1024@gmail.com>
2022-11-01 23:50:52 +0900

    [Add] axiosセットアップ

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    'plugins/axios'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,8 +42,6 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
