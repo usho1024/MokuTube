095a7e623e6d350c077c1acd4cd5d24917340a1e
usho1024 <usho1024@gmail.com>
2022-11-03 23:54:53 +0900

    [Add] タイムゾーンの設定

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,9 +28,14 @@ module App
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Railsアプリデフォルトのタイムゾーン(default 'UTC')
+    # TimeZoneList: http://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html
+    config.time_zone = ENV["TZ"]
+
+    # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
+    config.active_record.default_timezone = :utc
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
