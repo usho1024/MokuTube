6877027d3bf11514585047865978b915ebe6b6b5
usho1024 <usho1024@gmail.com>
2022-11-04 23:36:03 +0900

    [Add] i18nとオートロードパスの設定

--- api/config/application.rb
+++ api/config/application.rb
@@ -37,6 +37,12 @@ module App
     # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
     config.active_record.default_timezone = :utc
 
+    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
+    config.i18n.default_locale = :ja
+
+    # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
+    config.add_autoload_paths_to_load_path = false
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
