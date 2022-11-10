b4626e9dddc967f8491b294a32e2fcfb559b965f
usho1024 <usho1024@gmail.com>
2022-11-11 00:00:07 +0900

    [Add] フロントだけで席移動の機能追加

---
+++ front/components/UserAvatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-avatar
+    class="avatar"
+    color="primary"
+    size="50"
+    :style="{
+      transform: `translate(${x}px, ${y}px)`
+    }"
+  ></v-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    x: { type: Number, default: 100 },
+    y: { type: Number, default: 100 }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <v-btn />
+    <SvgTest />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
 }
 </script>
