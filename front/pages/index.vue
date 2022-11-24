151c5d4906d6fe0d3ff82d2d54432eee94834ba6
usho1024 <usho1024@gmail.com>
2022-11-24 18:54:00 +0900

    feat: API通信の確認のため必要ファイルを作成

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,16 +1,53 @@
 <template>
-  <div class="main">
-    <RoomSmallOffice />
+  <div>
+    <h2>
+      Usersテーブルの取得
+    </h2>
+    <table v-if="users.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(user, i) in users"
+          :key="`user-${i}`"
+        >
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ dateFormat(user.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-else>
+      ユーザーが取得できませんでした
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
-</script>
-
-<style lang="scss" scoped>
-.main {
-  height: 100vh;
-  background-color: #b3e5fc;
+export default {
+  async asyncData ({ $axios }) {
+    let users = []
+    await $axios.$get('/api/v1/users')
+      .then(res => (users = res))
+    return { users }
+  },
+  computed: {
+    dateFormat () {
+      return (date) => {
+        const dateTimeFormat = new Intl.DateTimeFormat(
+          'ja', { dateStyle: 'medium', timeStyle: 'short' }
+        )
+        return dateTimeFormat.format(new Date(date))
+      }
+    }
+  }
 }
-</style>
+</script>
---
+++ front/pages/room/_id/room.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <RoomSmallOffice />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  height: 100vh;
+  background-color: #b3e5fc;
+}
+</style>
