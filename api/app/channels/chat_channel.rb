83ffc9c77635a0ba2ba7e05854b06dc92e8ad7d7
usho1024 <usho1024@gmail.com>
2022-12-10 09:24:41 +0900

    feat: #14 仮のチャット機能を追加

--- api/app/channels/chat_channel.rb
+++ api/app/channels/chat_channel.rb
@@ -3,11 +3,16 @@ class ChatChannel < ApplicationCable::Channel
     stream_from("chat:#{params[:room]}")
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
-  def test
-    ActionCable.server.broadcast("chat:#{params[:room]}", { body: "「#{params[:room]}」を購読中です" })
+  # チャットルームにメッセージを配信
+  def speak(data)
+    content = {
+      type: 'speak',
+      body: {
+        name: data['name'],
+        message: data['message'],
+        spoke_at: Time.zone.now,
+      },
+    }
+    ActionCable.server.broadcast("chat:#{params[:room]}", content)
   end
 end
--- front/pages/test.vue
+++ front/pages/test.vue
@@ -1,6 +1,53 @@
 <template>
   <div>
-    <button @click="test">テスト</button>
+    <v-row>
+      <v-col
+        cols="6"
+      >
+        <!-- メッセージ一覧 -->
+        <div
+          v-for="(message, i) in messages"
+          :key="`message-${i}`"
+        >
+          {{ message.name }}：  {{ message.message }}
+          <small>{{ message.spoke_at }}</small>
+        </div>
+      </v-col>
+      <v-col
+        cols="6"
+      >
+        <!-- 入力 -->
+        <v-row justify="center">
+          <v-col
+            cols="6"
+          >
+            <h2>名前</h2>
+              <v-text-field
+                v-model="name"
+                type="text"
+              />
+          </v-col>
+        </v-row>
+
+        <v-row justify="center">
+          <v-col
+            cols="6"
+          >
+            <h2>内容</h2>
+              <v-text-field
+                v-model="input_message"
+                type="text"
+              />
+              <v-btn
+                class="primary"
+                block
+                @click="speak">
+                投稿
+              </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -9,24 +56,39 @@ import ActionCable from 'actioncable'
 
 export default {
   auth: false,
+  data() {
+    return {
+      name: '',
+      input_message: '',
+      messages: []
+    }
+  },
   created() {
+    const messages = this.messages
     // 接続を生成（引数は、'ws://[Railsの接続URL]/cable'
     const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
     // chatチャンネルのサブスクリプションを作成
     this.chatChannel = cable.subscriptions.create(
       { channel: 'ChatChannel', room: 'チャットルーム１' },
       {
-        // 配信された時のメソッド
-        received(response) {
-          console.log(response)
+        received({ type, body }) {
+          switch (type) {
+            case 'speak':
+              messages.push(body)
+              break
+          }
         }
       }
     )
   },
   methods: {
-    // 接続確認
-    test() {
-      this.chatChannel.perform('test')
+    // performの第二引数でサーバー側の関数の引数を設定できる
+    speak() {
+      this.chatChannel.perform('speak', {
+        name: this.name,
+        message: this.input_message,
+      })
+      this.input_message = ''
     }
   }
 }
