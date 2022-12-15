77db7666fd619a574edeaaaaedce8b0323faf597
usho1024 <usho1024@gmail.com>
2022-12-16 05:04:31 +0900

    feat: #14 微修正

--- api/app/controllers/api/v1/messages_controller.rb
+++ api/app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     messages = Message.where(room_id: params[:id]).limit(50).reverse_order.includes(:user)
     messages.map { |message| message[:name] = message.user.name }
--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    render json: users
   end
 end
