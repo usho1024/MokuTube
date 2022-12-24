0e8cd50999e30a8d1e0c2e86fcbe077a0a88048c
usho1024 <usho1024@gmail.com>
2022-12-25 03:45:20 +0900

    test: #16 ルームに関するテストを追加

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { 'テストユーザー' }
-    email { 'test@example.com' }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { 'password' }
   end
 end
--- api/spec/requests/api/v1/rooms_users_spec.rb
+++ api/spec/requests/api/v1/rooms_users_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "RoomsUsersAPI", type: :request do
-  describe "GET /index" do
+  describe 'GET /api/v1/rooms_users' do
+    let(:room_image) { create(:room_image) }
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:room) { create(:room, room_image: room_image, user: user1) }
+    let(:auth_tokens) { sign_in(user1) }
+
+    it 'ルームに存在するユーザーを取得できること' do
+      create(:rooms_user, user: user1, room: room, seat_number: 1 )
+      create(:rooms_user, user: user2, room: room, seat_number: 2 )
+      get api_v1_rooms_users_path, headers: auth_tokens, params: { id: room.id }
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(json.length).to eq(2)
+    end
   end
 end
