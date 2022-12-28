d42428d2570a8f9e7bd24e79e6da7467152b3497
usho1024 <usho1024@gmail.com>
2022-12-29 02:43:58 +0900

    refactor: #18 チャットのメッセージをコンポーネント化

---
+++ front/components/ListMessage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="px-4 mb-4"
+  >
+    <v-row
+      no-gutters
+    >
+      <v-col
+        cols="1"
+        align-self="start"
+      >
+        <v-avatar
+          size="35px"
+        >
+          <v-img :src="avatar"/>
+        </v-avatar>
+      </v-col>
+      <v-col
+        cols="11"
+      >
+        <div
+          class="ml-3"
+        >
+          <span
+            class="grey--text text--darken-1 mr-2"
+          >
+            {{ name }}
+          </span>
+          <span
+            class="grey--text text--darken-4"
+          >
+            {{ body }}
+          </span>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    body: {
+      type: String,
+      default: null
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    avatar: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
--- front/pages/rooms/_id.vue
+++ front/pages/rooms/_id.vue
@@ -54,47 +54,13 @@
               class="overflow-y-auto grey lighten-5 text-caption font-weight-medium pt-4"
               height=70%
             >
-              <template
+              <list-message
                 v-for="(message, i) in chatMessages"
-              >
-                <div
-                  :key="`message-${i}`"
-                  class="px-4 mb-4"
-                >
-                  <v-row
-                    no-gutters
-                  >
-                    <v-col
-                      cols="1"
-                      align-self="start"
-                    >
-                      <v-avatar
-                        size="35px"
-                      >
-                        <v-img :src="message.sender.avatar"/>
-                      </v-avatar>
-                    </v-col>
-                    <v-col
-                      cols="11"
-                    >
-                      <div
-                        class="ml-3"
-                      >
-                        <span
-                          class="grey--text text--darken-1 mr-2"
-                        >
-                          {{ message.sender.name }}
-                        </span>
-                        <span
-                          class="grey--text text--darken-4"
-                        >
-                          {{ message.body }}
-                        </span>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </div>
-              </template>
+                :key="`message-${i}`"
+                :body="message.body"
+                :name="message.sender.name"
+                :avatar="message.sender.avatar"
+              />
             </v-sheet>
 
             <v-divider/>
