921b17acaa09f4a72607208c0f4ffeeaa08f4bd2
usho1024 <usho1024@gmail.com>
2023-01-18 16:46:01 +0900

    feat: #24 編集ダイアログ完成

--- front/components/Dialog/DialogUserEdit.vue
+++ front/components/Dialog/DialogUserEdit.vue
@@ -1,29 +1,57 @@
 <template>
-  <v-dialog v-model="dialog" width="500">
+  <v-dialog v-model="dialog" width="30%">
     <template #activator="{ on }">
-      <v-btn outlined color="indigo" dark v-on="on"
+      <v-btn outlined color="indigo" v-on="on"
         ><v-icon class="mr-2">mdi-account-edit</v-icon
         >プロフィールを編集する</v-btn
       >
     </template>
 
     <v-card>
-      <v-file-input
-        v-model="inputFile"
-        accept="image/png, image/jpeg"
-        label="アバターイメージをアップロードする"
-        chips
-        show-size
-        prepend-icon="mdi-camera"
-      />
-      <v-btn color="primary" :disabled="disabled" @click="updateUser">
-        更新する
-      </v-btn>
+      <v-subheader>プロフィールを編集</v-subheader>
+
+      <v-divider />
+
+      <v-sheet class="pa-5">
+        <v-form>
+          <v-text-field
+            v-model="name"
+            counter="30"
+            label="ユーザーネーム"
+            class="mb-5"
+          ></v-text-field>
+          <v-textarea
+            v-model="introduction"
+            counter="300"
+            label="自己紹介文"
+            class="mb-5"
+          ></v-textarea>
+          <v-text-field
+            v-model="work"
+            counter="30"
+            label="作業内容"
+            class="mb-5"
+          ></v-text-field>
+          <v-file-input
+            v-model="inputFile"
+            accept="image/png, image/jpeg"
+            label="アバターイメージをアップロードする"
+            chips
+            show-size
+            prepend-icon="mdi-camera"
+          />
+        </v-form>
+      </v-sheet>
 
       <v-divider />
 
       <v-card-actions>
-        <v-btn color="primary" text @click="dialog = false">閉じる</v-btn>
+        <v-btn color="primary" width="100" class="mr-3" @click="updateUser">
+          更新する
+        </v-btn>
+        <v-btn color="primary" text width="100" @click="dialog = false"
+          >閉じる</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -32,29 +60,33 @@
 <script>
 export default {
   props: {
-    introduction: {
-      type: String,
+    user: {
+      type: Object,
       default: null,
     },
   },
   data() {
     return {
       dialog: false,
+      name: this.user.name,
+      introduction: this.user.introduction,
+      work: this.user.work,
       inputFile: null,
     }
-  },
-  computed: {
-    disabled() {
-      return !this.inputFile
-    },
   },
   methods: {
     async updateUser() {
       const formData = new FormData()
-      formData.append('user[avatar]', this.inputFile)
+      formData.append('user[name]', this.name)
+      formData.append('user[introduction]', this.introduction)
+      formData.append('user[work]', this.work)
+      if (this.inputFile) {
+        formData.append('user[avatar]', this.inputFile)
+      }
       await this.$axios
-        .patch(`/api/v1/users/${this.currentUser.id}`, formData)
+        .patch(`/api/v1/users/${this.user.id}`, formData)
         .then((response) => {
+          this.$emit('update:user', response.data)
           const user = {
             id: response.data.id,
             name: response.data.name,
@@ -62,6 +94,7 @@ export default {
           }
           this.$store.dispatch('getCurrentUser', user)
           this.inputFile = null
+          this.dialog = false
         })
     },
   },
--- front/pages/mypage.vue
+++ front/pages/mypage.vue
@@ -8,29 +8,39 @@
             :src="image_src"
             gradient="rgba(100,115,201,.33), rgba(25,32,72,.33)"
           >
-            <v-card-title class="white--text mt-10 ml-2">
+            <v-card-title class="white--text font-weight-bold mt-10 ml-2">
               <v-avatar size="100">
                 <img :src="user.avatar.url" />
               </v-avatar>
-              <p class="ml-3">{{ user.name }}（{{ user.email }}）</p>
+              <p class="ml-3">{{ user.name }}</p>
             </v-card-title>
           </v-img>
 
           <div class="pa-7">
-            <p>
-              {{ user.introduction }}
-            </p>
+            <div class="mb-8">
+              <span v-if="user.introduction">
+                {{ user.introduction }}
+              </span>
+              <span v-else>自己紹介文が設定されていません</span>
+            </div>
             <div class="grey--text text--darken-2 mb-7">
               <p>
-                <v-icon class="mr-2">mdi-lead-pencil</v-icon>
-                {{ user.work }}
+                <v-icon class="mr-3">mdi-lead-pencil</v-icon>
+                <span v-if="user.work">
+                  {{ user.work }}
+                </span>
+                <span v-else>作業内容が設定されていません</span>
               </p>
               <p>
-                <v-icon class="mr-2">mdi-database-clock</v-icon>
+                <v-icon class="mr-3">mdi-database-clock</v-icon>
                 {{ secToTime(user.total_stay_time) }}
               </p>
               <p>
-                <v-icon class="mr-2">mdi-account-tag</v-icon>
+                <v-icon class="mr-3">mdi-email</v-icon>
+                {{ user.email }}
+              </p>
+              <p>
+                <v-icon class="mr-3">mdi-account-tag</v-icon>
                 <span v-if="user.guest"
                   >ゲストユーザー
                   <v-tooltip top>
@@ -47,12 +57,12 @@
                 <span v-else>一般ユーザー</span>
               </p>
               <p>
-                <v-icon class="mr-2">mdi-calendar</v-icon>
+                <v-icon class="mr-3">mdi-calendar</v-icon>
                 {{ dateFormat(user.created_at) }}からMokuTubeを利用しています
               </p>
             </div>
 
-            <dialog-user-edit />
+            <dialog-user-edit :user.sync="user" />
           </div>
         </v-card>
       </v-col>
