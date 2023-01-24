3cf4ba98e2d25badb46e9ba8f55a6b74e1c3d5ae
usho1024 <usho1024@gmail.com>
2023-01-24 17:58:12 +0900

    docs: #26 ボタンの見た目に問題があったのでダイアログの記述を修正

--- front/components/Dialog/DialogIntroduction.vue
+++ front/components/Dialog/DialogIntroduction.vue
@@ -1,24 +1,26 @@
 <template>
-  <v-dialog v-model="dialog" width="500">
-    <template #activator="{ on }">
-      <v-btn small color="primary" dark v-on="on">自己紹介文を見る</v-btn>
-    </template>
+  <div>
+    <v-btn small color="primary" dark @click="dialog = true"
+      >自己紹介文を見る</v-btn
+    >
 
-    <v-card>
-      <v-card-text v-if="introduction" class="pa-5">{{
-        introduction
-      }}</v-card-text>
-      <v-card-text v-else class="pa-5"
-        >このユーザーは自己紹介文を設定していません</v-card-text
-      >
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-text v-if="introduction" class="pa-5">{{
+          introduction
+        }}</v-card-text>
+        <v-card-text v-else class="pa-5"
+          >このユーザーは自己紹介文を設定していません</v-card-text
+        >
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions>
-        <v-btn color="appblue" text @click="dialog = false">閉じる</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+        <v-card-actions>
+          <v-btn color="appblue" text @click="dialog = false">閉じる</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
--- front/components/Dialog/DialogUserEdit.vue
+++ front/components/Dialog/DialogUserEdit.vue
@@ -1,82 +1,82 @@
 <template>
-  <v-dialog v-model="dialog" width="30%">
-    <template #activator="{ on }">
-      <v-btn outlined color="indigo" v-on="on"
-        ><v-icon class="mr-2">mdi-account-edit</v-icon
-        >プロフィールを編集する</v-btn
-      >
-    </template>
+  <div>
+    <v-btn outlined color="indigo" @click="dialog = true"
+      ><v-icon class="mr-2">mdi-account-edit</v-icon
+      >プロフィールを編集する</v-btn
+    >
 
-    <v-card>
-      <v-subheader>プロフィールを編集</v-subheader>
+    <v-dialog v-model="dialog" width="30%">
+      <v-card>
+        <v-subheader>プロフィールを編集</v-subheader>
 
-      <v-divider />
+        <v-divider />
 
-      <v-sheet class="pa-5">
-        <v-form>
-          <v-row justify="center" class="my-5">
-            <v-avatar size="100">
-              <img :src="url" />
-              <v-overlay absolute>
-                <v-tooltip bottom>
-                  <template #activator="{ on }">
-                    <div v-on="on">
-                      <v-file-input
-                        v-model="inputFile"
-                        accept="image/png, image/jpeg"
-                        hide-input
-                        prepend-icon="mdi-camera-plus-outline"
-                        class="mb-3 ml-2"
-                      />
-                    </div>
-                  </template>
-                  <span>画像を追加</span>
-                </v-tooltip>
-              </v-overlay>
-            </v-avatar>
-          </v-row>
+        <v-sheet class="pa-5">
+          <v-form>
+            <v-row justify="center" class="my-5">
+              <v-avatar size="100">
+                <img :src="url" />
+                <v-overlay absolute>
+                  <v-tooltip bottom>
+                    <template #activator="{ on }">
+                      <div v-on="on">
+                        <v-file-input
+                          v-model="inputFile"
+                          accept="image/png, image/jpeg"
+                          hide-input
+                          prepend-icon="mdi-camera-plus-outline"
+                          class="mb-3 ml-2"
+                        />
+                      </div>
+                    </template>
+                    <span>画像を追加</span>
+                  </v-tooltip>
+                </v-overlay>
+              </v-avatar>
+            </v-row>
 
-          <v-text-field
-            v-model="name"
-            counter="30"
-            label="ユーザーネーム"
-            class="mb-5"
-          />
+            <v-text-field
+              v-model="name"
+              counter="30"
+              label="ユーザーネーム"
+              class="mb-5"
+            />
 
-          <v-textarea
-            v-model="introduction"
-            counter="300"
-            label="自己紹介文"
-            class="mb-5"
-          />
+            <v-textarea
+              v-model="introduction"
+              counter="300"
+              label="自己紹介文"
+              class="mb-5"
+            />
 
-          <v-text-field
-            v-model="work"
-            counter="30"
-            label="作業内容"
-            class="mb-2"
-          />
-        </v-form>
-      </v-sheet>
+            <v-text-field
+              v-model="work"
+              counter="30"
+              label="作業内容"
+              class="mb-2"
+            />
+          </v-form>
+        </v-sheet>
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions>
-        <v-btn
-          dark
-          color="appblue"
-          width="100"
-          class="mr-3"
-          @click="updateUser"
-        >
-          更新する
-        </v-btn>
-        <v-btn color="appblue" text width="100" @click="dialog = false"
-          >閉じる</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+        <v-card-actions>
+          <v-btn
+            dark
+            color="appblue"
+            width="100"
+            class="mr-3"
+            @click="updateUser"
+          >
+            更新する
+          </v-btn>
+          <v-btn color="appblue" text width="100" @click="dialog = false"
+            >閉じる</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
