e1504ce48dd5ec9781f003f9c36a2b5cba03d000
usho1024 <usho1024@gmail.com>
2023-02-05 06:00:52 +0900

    refactor: #32 Roomのscopeを見やすくした

--- api/app/models/room.rb
+++ api/app/models/room.rb
@@ -14,7 +14,14 @@ class Room < ApplicationRecord
       allow_blank: true
     }
 
-    scope :active, -> (page_number) { includes([:user, :room_image]).find(RoomsUser.group(:room_id).order('count(room_id) desc').pluck(:room_id)).slice(30 * (page_number - 1), 30) }
-    scope :recent, -> (page_number) { order(created_at: :desc).includes([:user, :room_image]).slice(30 * (page_number - 1), 30) }
-    scope :count_active, -> { find(RoomsUser.group(:room_id).pluck(:room_id)).count }
+    scope :active, -> (page_number) {
+      includes([:user, :room_image]).find(RoomsUser.group(:room_id)
+      .order('count(room_id) desc').pluck(:room_id)).slice(30 * (page_number - 1), 30)
+    }
+    scope :recent, -> (page_number) {
+      includes([:user, :room_image]).order(created_at: :desc).slice(30 * (page_number - 1), 30)
+    }
+    scope :count_active, -> {
+      find(RoomsUser.group(:room_id).pluck(:room_id)).count
+    }
 end
