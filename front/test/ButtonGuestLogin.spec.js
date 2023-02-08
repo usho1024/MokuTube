a07f69c607d0f870f198d08541f2a6baca9b5500
usho1024 <usho1024@gmail.com>
2023-02-09 05:21:07 +0900

    test: #32 vuetify用の設定を追加

--- front/test/ButtonGuestLogin.spec.js
+++ front/test/ButtonGuestLogin.spec.js
@@ -4,6 +4,6 @@ import ButtonGuestLogin from '@/components/Button/ButtonGuestLogin.vue'
 describe('ButtonGuestLogin', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(ButtonGuestLogin)
-    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.html()).toContain('ゲストログインして今すぐ使ってみる！')
   })
 })
