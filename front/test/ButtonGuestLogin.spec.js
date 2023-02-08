1a1d2ca49fce61a17eff0bbfa7f2a0342a225e69
usho1024 <usho1024@gmail.com>
2023-02-08 23:22:32 +0900

    docs: #32 不要なコンポーネントを削除

---
+++ front/test/ButtonGuestLogin.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import ButtonGuestLogin from '@/components/Button/ButtonGuestLogin.vue'
+
+describe('ButtonGuestLogin', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(ButtonGuestLogin)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
