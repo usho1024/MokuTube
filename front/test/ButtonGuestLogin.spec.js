9f1b34b7f14435cf0f650b03a2559c2a0b207d30
usho1024 <usho1024@gmail.com>
2023-02-11 06:23:39 +0900

    test: #32 Jestのテスト追加

--- front/test/ButtonGuestLogin.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import ButtonGuestLogin from '@/components/Button/ButtonGuestLogin.vue'
-
-describe('ButtonGuestLogin', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(ButtonGuestLogin)
-    expect(wrapper.html()).toContain('ゲストログインして今すぐ使ってみる！')
-  })
-})
