af759774f7d8ac23d77d2d0279c3c5ec78dab5ba
usho1024 <usho1024@gmail.com>
2023-02-18 04:46:09 +0900

    refactor: #35 docker関連ファイルを修正

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -49,7 +49,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http:localhost:3000',
+    baseURL: 'http://localhost:3000',
   },
 
   auth: {
